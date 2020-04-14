833cec977b76d03b5153fcaada9055ed3b84cc27
marcelo.zanol <marcelo.zanol@focco.com.br>
2020-04-14 19:04:29 -0300

    Add: home and cart pages, fake api, redux config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+
+import store from './store';
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>Hello</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
