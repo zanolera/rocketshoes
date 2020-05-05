d548f1a923bec3d5b7d6910d30531707be17bb78
marcelo.zanol <marcelo.zanol@focco.com.br>
2020-05-04 21:10:25 -0300

    Add Reactotron saga config

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,10 +24,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = (product) => {
-    const { addToCart } = this.props;
+  handleAddProduct = (id) => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -45,7 +45,7 @@ class Home extends Component {
             <button
               type="button"
               onClick={() => {
-                this.handleAddProduct(product);
+                this.handleAddProduct(product.id);
               }}
             >
               <div>
